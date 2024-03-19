f8e1ed356de7973e62baf32eb770b0a4f90fd1b5
JoseVasqueZ10 <javasquez747@gmail.com>
2024-03-18 21:02:37 -0500

    Hemos finalizado la sesion 2

---
+++ script.js
@@ -0,0 +1,2 @@
+alert("¿Sabes que puedes desarrollar con Flutter?");
+alert("Aplicaciones móvoles\n Aplicaciones Web\n Aplicaciones de Escritorio\n Aplicaciones para Dispositivos Integrados y IOT\n Aplicaciones de Realida Aumentada (AR) y realidad virtual (VR)\n Juegos\n Aplicaciones empresariales y corporativas\n Aplicaciones educativas");
